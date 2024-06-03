024d6365836b775fb78545ece3dcd54668f5e178
tigaweb <nka.webtech.dev@gmail.com>
2024-06-03 23:44:33 +0900

    backend: DBの初期化処理を実装

---
+++ backend/db/db.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func NewDB() *gorm.DB {
+	if os.Getenv("GO_ENV") == "development" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+        os.Getenv("MYSQL_USER"),
+        os.Getenv("MYSQL_PW"),
+        os.Getenv("MYSQL_HOST"),
+        os.Getenv("MYSQL_PORT"),
+        os.Getenv("MYSQL_DB"),
+    )
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Database Connected")
+	return db
+}
+
+func CloseDB(db *gorm.DB)  {
+	sqlDB, _ := db.DB()
+	if err := sqlDB.Close(); err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ backend/migrate/migrate.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tigaweb/reversi-app/backend/db"
+	"github.com/tigaweb/reversi-app/backend/model"
+)
+
+func main()  {
+	dbConn :=  db.NewDB();
+	defer fmt.Println("Successfully Migrated")
+	defer db.CloseDB(dbConn)
+	dbConn.AutoMigrate(&model.User{})
+}
